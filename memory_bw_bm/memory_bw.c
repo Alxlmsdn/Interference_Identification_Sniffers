e3ebbae9dbe48bab69367d140c65ec7fc7251a14
Alexander Lumsden <alexlumsden@Alexanders-Mac-mini.local>
2019-06-29 17:18:22 +1200

    added cpu affinity for testing

--- memory_bw_bm/memory_bw.c
+++ memory_bw_bm/memory_bw.c
@@ -10,7 +10,7 @@
 //      determining the bandwidth of cache levels. 
 //
 
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -20,11 +20,16 @@
 #include <limits.h>
 #include <cctimer.h>
 #include <pthread.h>
+#include <sched.h>
 //#include <sys/sysinfo.h>
+
+#define DEBUG
 
 #define STREAM_ELEMENTS double
 
-static STREAM_ELEMENTS a,b,c;
+void* setAffinity();
+
+static STREAM_ELEMENTS *a, *b, *c;
 static double average_times[4] = {0};
 static double num_bytes[4];
 
@@ -35,16 +40,54 @@ int main(int argc, char* argv[]) {
     //fprintf(stdout, "first: %f\n second: %f\n difference: %f", first, second, (second - first) * 1e3);
     //return 0;
     if (argc != 4) {
-        fprintf(stderr, "Usage: <number of threads> <array size> <number of iterations>");
+        fprintf(stderr, "Usage: <number of threads> <array size> <number of iterations>\n");
         return 1;
     }
     uint32_t num_threads = atoi(argv[1]);
     uint32_t array_size = atoi(argv[2]);
     uint32_t iterations = atoi(argv[3]);
 
-    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
-    fprintf(stdout, "number of cores: %d\n", numCPU);
+    a = (STREAM_ELEMENTS *) malloc(sizeof(STREAM_ELEMENTS) * array_size);
+    b = (STREAM_ELEMENTS *) malloc(sizeof(STREAM_ELEMENTS) * array_size);
+    c = (STREAM_ELEMENTS *) malloc(sizeof(STREAM_ELEMENTS) * array_size);
+
+    num_bytes[0] = 2 * sizeof(STREAM_ELEMENTS) * array_size;
+    num_bytes[1] = 2 * sizeof(STREAM_ELEMENTS) * array_size;
+    num_bytes[2] = 3 * sizeof(STREAM_ELEMENTS) * array_size;
+    num_bytes[3] = 3 * sizeof(STREAM_ELEMENTS) * array_size;
+
+    int num_cores_available = sysconf(_SC_NPROCESSORS_ONLN);
+    int num_cores_total = sysconf(_SC_NPROCESSORS_CONF);
+
+#ifdef DEBUG
+    fprintf(stdout, "number of cores available: %d, from total cores: %d", num_cores_available, num_cores_total);
+#endif
+
+    pthread_t threads[num_cores_total];
+    uint32_t thread_id[num_cores_total];
+
+    for (uint32_t i = 0 ; i < num_cores_total; i ++) {
+        thread_id[i] = i;
+        pthread_create(&threads[i], NULL, setAffinity, &thread_id[i]);
+    }
+    for(uint32_t i = 0; i < num_cores_total; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
+}
 
+
+void* setAffinity(int* id) {
+    uint32_t thread_id = *(uint32_t *) id;
+    pthread_t thread = pthread_self();
+    cpu_set_t* cpu = CPU_ALLOC(1);
+    int size = CPU_ALLOC_SIZE(1);
+    CPU_ZERO_S(size, cpu);
+    CPU_SET_S(thread_id, size, cpu);
+    int s = pthread_setaffinity_np(thread, size, cpu);
+    if (s != 0) {
+        pthread_exit((void *) 1);
+    }
+    fprintf(stdout, "Thread %d succeded\n", thread_id);
 }
