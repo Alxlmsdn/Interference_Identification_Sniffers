016470270c478bd986119c0e8d9ef27158f64830
Alex <alxlmsdn@gmail.com>
2019-07-18 08:53:52 +1200

    added timing and data output to network_bw bm

--- network_bw_bm/network_bw.c
+++ network_bw_bm/network_bw.c
@@ -23,18 +23,19 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <cctimer.h>
 
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 4) {
-        fprintf(stderr, "Usage: <url/IP> <port> <messageSize>\n");
+    if (argc < 5) {
+        fprintf(stderr, "Usage: <url/IP> <port> <messageSize> <runTime>\n");
         return -1;
     }
 
     int port = atoi(argv[2]);
     int msg_length = atoi(argv[3]);
+    double runTime = atof(argv[4]);
     char *ip = argv[1];
     char *msg_buff = (char *) malloc(sizeof(char) * msg_length);
     for (uint32_t i=0; i < msg_length; i++) {
@@ -59,19 +60,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int written = 0, ret;
-    char *ptr = msg_buff;
-
-    while (written < msg_length) {
-        ret = write(socketfd, ptr, msg_length - written);
-        if (ret < 0) {
-            fprintf(stdout, "Error writing file\n");
-            perror("writing to file"); 
-            pthread_exit((void *) 1);
+    double elapsed_time = 0;//cc_get_seconds(0);
+    while (elapsed_time < runTime) {
+        int written = 0, ret;
+        char *ptr = msg_buff;
+        double start_time = cc_get_seconds(0);
+        while (written < msg_length) {
+            ret = write(socketfd, ptr, msg_length - written);
+            if (ret < 0) {
+                fprintf(stdout, "Error writing file\n");
+                perror("writing to file"); 
+                pthread_exit((void *) 1);
+            }
+            written += ret;
+            ptr += ret;
         }
-        written += ret;
-        ptr += ret;
+        double stop_time = cc_get_seconds(0) - start_time;
+        elapsed_time += stop_time;
+        fprintf(stdout, "thread:[0],rate:[%f](MB/s),totalTime:[%f](s)\n", (1e-6 *(sizeof(char)*msg_length))/stop_time, elapsed_time); 
     }
+    
 
     close(socketfd);
     free(msg_buff);
